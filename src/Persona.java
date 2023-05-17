db94bf574980acbf9e42d6cdefa184fd963cdc00
Steffy-77-36 <sthefany.lucio@epn.edu.ec>
2023-05-17 14:52:58 -0500

    Listo para liberar

--- src/Persona.java
+++ src/Persona.java
@@ -1,3 +1,7 @@
 public class Persona {
+    public  Persona (){
+        System.out.println("Soy persona");
+        System.out.println("I have an issue");
+    }
     
 }
---
+++ src/SuperAnimal.java
@@ -0,0 +1,7 @@
+public class SuperAnimal {
+    public SuperAnimal (){
+        System.out.println("Soy animal xxxxx");
+        System.out.println("I am Buggggg");
+    }
+    
+}
